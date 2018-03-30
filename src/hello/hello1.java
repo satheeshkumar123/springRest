a0fbe0a826173d50d20505e5f5cb521c4340bfaf
darshumccoy <darshinisheku@gmail.com>
2018-03-30 12:49:17 +0530

    commited recently

---
+++ src/hello/hello1.java
@@ -0,0 +1,19 @@
+package hello;
+
+public class hello1 {
+	
+	public static void main(String[] args) {
+		int n=10;
+		
+		for( int i=0;i<n;i++)
+		{
+			
+			System.out.println("hello world how r u???");
+			
+		}
+		
+	}
+	
+	
+
+}
